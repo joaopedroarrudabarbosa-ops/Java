6969d3af9219249ab9c48ce3bab3d8bf9154a7a8
João Pedro <joaopedroarrudabarbosa@gmail.com>
2026-08-19 13:16:28 -0300

    feat: add sistema emprestimo

---
+++ src/SistemaEmprestimo.java
@@ -0,0 +1,14 @@
+public class SistemaEmprestimo {
+    public static void main(String[]args) {
+        int idade = 20;
+        int salario = 2500;
+        boolean nomeNegativado = false;
+
+        if (idade >= 18 && salario >= 2000 && !nomeNegativado) {
+            System.out.println("Empréstimo aprovado!");
+        }
+        else {
+            System.out.println("Empréstimo Negado!");
+        }
+    }
+}
