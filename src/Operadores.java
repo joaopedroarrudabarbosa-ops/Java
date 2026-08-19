0d146a04e0d20704edcab1d6e39330e86fcdfa52
João Pedro <joaopedroarrudabarbosa@gmail.com>
2026-08-19 12:21:28 -0300

    add arithmetic operators

---
+++ src/Operadores.java
@@ -0,0 +1,18 @@
+public class Operadores {
+    public static void main(String[]args) {
+        int numero1 = 20;
+        int numero2 = 6;
+        int soma = numero1 + numero2;
+        System.out.println(soma);
+
+
+        int subtracao = numero1 - numero2;
+        System.out.println(subtracao);
+
+        int multiplicacao = numero1 * numero2;
+        System.out.println(multiplicacao);
+
+        double divisao = (double) numero1 / numero2; // casting
+        System.out.println(divisao);
+    }
+}
