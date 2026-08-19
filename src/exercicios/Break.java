a0e1e07b8a01d31e975ae63824ef07c48e3fb52a
João Pedro <joaopedroarrudabarbosa@gmail.com>
2026-08-19 17:59:11 -0300

    feat: add loops and exercises

---
+++ src/exercicios/Break.java
@@ -0,0 +1,12 @@
+package exercicios;
+
+public class Break {
+    public static void main(String[]args) {
+        for (int i=1;i <=20; i++) {
+            System.out.println(i);
+            if (i == 10) {
+                break;
+            }
+        }
+    }
+}
---
+++ src/repeticoes/DoWhile.java
@@ -0,0 +1,13 @@
+// EXECUTA DEPOS VERIFICAR
+
+package repeticoes;
+
+public class DoWhile {
+    public static void main(String[]args) {
+        int numero = 1;
+        do {
+            System.out.println(numero);
+            numero++;
+        } while (numero <= 5);
+    }
+}
